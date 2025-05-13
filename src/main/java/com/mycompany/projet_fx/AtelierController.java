7a5ea03982c8c7f0295f0197f96d49b27b9a2d48
Matis <Matis@172.17.29.237>
2025-05-13 14:25:05 +0200

    creation depot

--- src/main/java/com/mycompany/projet_fx/AtelierController.java
+++ src/main/java/com/mycompany/projet_fx/AtelierController.java
@@ -1,38 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.projet_fx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuItem;
-import javafx.scene.input.MouseEvent;
-/**
- *
- * @author Matis
- */
+
 public class AtelierController {
+
     @FXML
     private ListView<String> listViewClasses;
-    
+
     @FXML
     private MenuItem menuItemOuvrir;
-    
+
     @FXML
-    public void initialize(){
-        listViewClasses.getItems().add("Classes A");
-        listViewClasses.getItems().add("Classes B");
-        listViewClasses.getItems().add("Classes C");
+    public void initialize() {
+        listViewClasses.getItems().addAll("Classe A", "Classe B", "Classe C");
     }
-    
+
     @FXML
-    public void handleOuvrirMenuItem(MouseEvent event) {
+    public void handleOuvrirMenuItem(ActionEvent event) {
         String selectedItem = listViewClasses.getSelectionModel().getSelectedItem();
         if (selectedItem != null) {
             System.out.println("Vous avez sélectionné : " + selectedItem);
         } else {
             System.out.println("Aucune classe sélectionnée !");
         }
-}
+    }
 }
