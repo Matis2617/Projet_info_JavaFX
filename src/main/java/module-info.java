c767ce824a486f301979a2e97f0151613b0e384a
Matis <Matis@172.17.29.237>
2025-05-13 15:24:17 +0200

    creation depot

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mycompany.projet_fx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.projet_fx to javafx.fxml;
-    exports com.mycompany.projet_fx;
-}
-
