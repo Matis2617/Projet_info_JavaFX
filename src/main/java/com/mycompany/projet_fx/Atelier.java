2c08989bec378111826c482639d2ce34b3fb5e6d
Matis2617 <matis.roecker@gmail.com>
2025-05-22 15:58:28 +0200

    Update Atelier.java

--- src/main/java/com/mycompany/projet_fx/Atelier.java
+++ src/main/java/com/mycompany/projet_fx/Atelier.java
@@ -1,79 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.projet_fx;
 
-/**
- *
- * @author Matis
- */
-import java.util.ArrayList;
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Atelier implements Serializable {
     private static final long serialVersionUID = 1L;
+
     private int id;
     private String nom;
-    ArrayList<Equipement> equipement;
-    ArrayList<Operateur> operateur;
+    private ArrayList<Equipement> equipement;
+    private ArrayList<Operateur> operateur;
     private ChefAtelier ChefAtelier;
-    ArrayList<Personne> personne;
+    private ArrayList<Personne> personne;
+    private ArrayList<Poste> postes = new ArrayList<>(); // Ajouté pour la persistance des postes
 
-    public ArrayList<Operateur> getOperateurs() {
-        return operateur;
-    }
-
-    public void setOperateurs(ArrayList<Operateur> operateur) {
-        this.operateur = operateur;
-    }
-
-        
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public ArrayList<Equipement> getEquipements() {
-        return equipement;
-    }
-
-    public void setEquipements(ArrayList<Equipement> equipement) {
-        this.equipement = equipement;
-    }
-
-     public Atelier(int id, String nom, ArrayList<Equipement> equipement,ArrayList<Operateur> operateur,ArrayList<Personne> personne) {
+    // Constructeur
+    public Atelier(int id, String nom, ArrayList<Equipement> equipement, ArrayList<Operateur> operateur, ArrayList<Personne> personne) {
         this.id = id;
         this.nom = nom;
         this.equipement = equipement;
         this.operateur = operateur;
         this.personne = personne;
     }
-     public ChefAtelier getChefAtelier() {
-        return ChefAtelier;
-    }
 
-    public void setChefAtelier(ChefAtelier chefAtelier) {
-        this.ChefAtelier = chefAtelier;
-    }
+    // Getters/Setters (ajoute ceux pour postes)
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
-    public ArrayList<Personne> getPersonnes() {
-        return personne;
-    }
+    public String getNom() { return nom; }
+    public void setNom(String nom) { this.nom = nom; }
 
-    public void setPersonnes(ArrayList<Personne> personne) {
-        this.personne = personne;
-    }
-    
+    public ArrayList<Equipement> getEquipements() { return equipement; }
+    public void setEquipements(ArrayList<Equipement> equipement) { this.equipement = equipement; }
+
+    public ArrayList<Operateur> getOperateurs() { return operateur; }
+    public void setOperateurs(ArrayList<Operateur> operateur) { this.operateur = operateur; }
+
+    public ChefAtelier getChefAtelier() { return ChefAtelier; }
+    public void setChefAtelier(ChefAtelier chefAtelier) { this.ChefAtelier = chefAtelier; }
+
+    public ArrayList<Personne> getPersonnes() { return personne; }
+    public void setPersonnes(ArrayList<Personne> personne) { this.personne = personne; }
+
+    public ArrayList<Poste> getPostes() { return postes; }
+    public void setPostes(ArrayList<Poste> postes) { this.postes = postes; }
 }
