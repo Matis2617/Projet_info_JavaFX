ae88be0e54c227a9b34ccbf05540cfa3a6412312
Matis2617 <matis.roecker@gmail.com>
2025-05-23 10:50:41 +0200

    Update StockBrutView.java

--- src/main/java/com/mycompany/projet_fx/View/StockBrutView.java
+++ src/main/java/com/mycompany/projet_fx/View/StockBrutView.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.projet_fx.View;
+package com.mycompany.projet_fx.view;
 
-/**
- *
- * @author Matis
- */
+import com.mycompany.projet_fx.controller.StockBrutController;
+import com.mycompany.projet_fx.model.Produit;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.*;
+
 public class StockBrutView {
-    
+    private VBox root;
+    private final StockBrutController controller;
+
+    public StockBrutView(StockBrutController controller) {
+        this.controller = controller;
+        createView();
+    }
+
+    private void createView() {
+        root = new VBox(15);
+        root.setStyle("-fx-padding: 30;");
+        Label titre = new Label("Stock Brut");
+        titre.setStyle("-fx-font-size: 18px; -fx-font-weight: bold;");
+        ListView<Produit> list = new ListView<>(controller.getProduitsBruts());
+        list.setPrefHeight(120);
+
+        // ... Ajout de produit brut
+
+        root.getChildren().addAll(titre, list /*, champs d'ajout, bouton ... */);
+    }
+
+    public VBox getView() { return root; }
 }
