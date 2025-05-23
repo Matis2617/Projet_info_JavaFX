883c63811859ce8b30da94e4752b4765e9e15f97
Matis2617 <matis.roecker@gmail.com>
2025-05-23 09:38:06 +0200

    Update Operateur.java

--- src/main/java/com/mycompany/projet_fx/Operateur.java
+++ src/main/java/com/mycompany/projet_fx/Operateur.java
@@ -1,53 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.projet_fx;
 
 import com.mycompany.projet_fx.Machine.ETAT;
 import java.io.Serializable;
-//permet d'utilis/*er ETAT dans cette classe
 
 /**
- *
- * @author Matis
+ * Classe modèle pour un opérateur (MVC friendly)
  */
 public class Operateur extends Personne implements Serializable {
     private String competences;
     private int id_op;
     private ETAT etat;
 
-    public String getCompetences() {
-        return competences;
-    }
-
-    public void setCompetences(String competences) {
-        this.competences = competences;
-    }
-
-    public int getId_op() {
-        return id_op;
-    }
-
-    public void setId_op(int id_op) {
-        this.id_op = id_op;
-    }
-
-    public ETAT getEtat() {
-        return etat;
-    }
-
-    public void setEtat(ETAT etat) {
-        this.etat = etat;
-    }
-
-    public Operateur(String idpersonne,String Nom, String Prenom, String competences, int id_op, ETAT etat) {
-        super(idpersonne,Nom,Prenom);
+    // ----- CONSTRUCTEUR -----
+    public Operateur(String idpersonne, String nom, String prenom, String competences, int id_op, ETAT etat) {
+        super(idpersonne, nom, prenom);
         this.competences = competences;
         this.id_op = id_op;
         this.etat = etat;
     }
-    @Override public String affiche(){
-    return super.affiche()+" competences = "+competences+", identifiant = "+id_op+", etat = "+etat;
-}
+
+    // ----- GETTERS / SETTERS -----
+    public String getCompetences() { return competences; }
+    public void setCompetences(String competences) { this.competences = competences; }
+
+    public int getId_op() { return id_op; }
+    public void setId_op(int id_op) { this.id_op = id_op; }
+
+    public ETAT getEtat() { return etat; }
+    public void setEtat(ETAT etat) { this.etat = etat; }
+
+    // ----- AFFICHAGE MVC (pour ListView, debug, etc) -----
+    @Override
+    public String toString() {
+        return super.toString()
+            + " | Compétences: " + competences
+            + " | Id Opérateur: " + id_op
+            + " | État: " + etat;
+    }
 }
