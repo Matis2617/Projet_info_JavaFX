0c7d8b89a53afbb64a02dc2815e7929a8d03e037
Matis2617 <matis.roecker@gmail.com>
2025-05-23 10:28:39 +0200

    Update ProduisFinisController.java

--- src/main/java/com/mycompany/projet_fx/Controller/ProduisFinisController.java
+++ src/main/java/com/mycompany/projet_fx/Controller/ProduisFinisController.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.projet_fx.Controller;
+package com.mycompany.projet_fx.controller;
 
-/**
- *
- * @author Matis
- */
-public class ProduisFinisController {
-    
+import com.mycompany.projet_fx.model.Produit;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class ProduitsFinisController {
+    private ObservableList<Produit> produitsFinis = FXCollections.observableArrayList();
+
+    public ObservableList<Produit> getProduitsFinis() {
+        return produitsFinis;
+    }
+
+    public void ajouterProduitFini(Produit produit) {
+        produitsFinis.add(produit);
+    }
+
+    public void supprimerProduitFini(Produit produit) {
+        produitsFinis.remove(produit);
+    }
 }
