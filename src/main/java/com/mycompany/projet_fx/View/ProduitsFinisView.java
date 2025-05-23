d201a931082c1e6725b622d087e47a353fa8d58f
Matis2617 <matis.roecker@gmail.com>
2025-05-23 10:50:29 +0200

    Update ProduitsFinisView.java

--- src/main/java/com/mycompany/projet_fx/View/ProduitsFinisView.java
+++ src/main/java/com/mycompany/projet_fx/View/ProduitsFinisView.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.projet_fx.View;
+package com.mycompany.projet_fx.view;
 
-/**
- *
- * @author Matis
- */
+import com.mycompany.projet_fx.controller.ProduitsFinisController;
+import com.mycompany.projet_fx.model.Produit;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.*;
+
 public class ProduitsFinisView {
-    
+    private VBox root;
+    private final ProduitsFinisController controller;
+
+    public ProduitsFinisView(ProduitsFinisController controller) {
+        this.controller = controller;
+        createView();
+    }
+
+    private void createView() {
+        root = new VBox(15);
+        root.setStyle("-fx-padding: 30;");
+        Label titre = new Label("Produits Finis");
+        titre.setStyle("-fx-font-size: 18px; -fx-font-weight: bold;");
+        ListView<Produit> list = new ListView<>(controller.getProduitsFinis());
+        list.setPrefHeight(120);
+
+        // ... Ajout de produit fini
+
+        root.getChildren().addAll(titre, list /*, champs d'ajout, bouton ... */);
+    }
+
+    public VBox getView() { return root; }
 }
