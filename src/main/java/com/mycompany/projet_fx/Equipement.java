2e01397c1ed8b2e631afd9a993a086cec16454a0
Matis2617 <matis.roecker@gmail.com>
2025-05-23 09:27:42 +0200

    Update Equipement.java

--- src/main/java/com/mycompany/projet_fx/Equipement.java
+++ src/main/java/com/mycompany/projet_fx/Equipement.java
@@ -1,16 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.projet_fx;
+package com.mycompany.projet_fx.model;
+
 import java.io.Serializable;
 
-/**
- *
- * @author Matis
- */
 public class Equipement implements Serializable {
     private int id_equipement;
+
+    public Equipement(int id_equipement) {
+        this.id_equipement = id_equipement;
+    }
 
     public int getId_equipement() {
         return id_equipement;
@@ -20,11 +17,7 @@ public class Equipement implements Serializable {
         this.id_equipement = id_equipement;
     }
 
-    public Equipement(int id_equipement) {
-        this.id_equipement = id_equipement;
-    }
-   
-   public String affiche(){
-    return "identifiant = "+id_equipement;
+    public String affiche() {
+        return "identifiant = " + id_equipement;
     }
 }
