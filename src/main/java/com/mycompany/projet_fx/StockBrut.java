c682d49c8ee4232f5914d4f526d6be098766097d
Matis2617 <matis.roecker@gmail.com>
2025-05-23 09:46:41 +0200

    Update StockBrut.java

--- src/main/java/com/mycompany/projet_fx/StockBrut.java
+++ src/main/java/com/mycompany/projet_fx/StockBrut.java
@@ -1,36 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.projet_fx;
+package com.mycompany.projet_fx.model;
 
-/**
- *
- * @author Matis
- */
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class StockBrut implements Serializable{
-   private ArrayList <Produit> produits;
-   
-   public StockBrut(){
-       this.produits = new ArrayList<>();
-   }
-   public void ajouterProduit (Produit produit){
-       produits.add(produit);
-   }
-   public void supprimerProduit (Produit produit){
-       produits.remove(produit);
-   }
-   public int getNombreProduits(){
-       return produits.size();
-   }
+public class StockBrut implements Serializable {
+    private ArrayList<Produit> produits;
 
-   public void afficherStock(){
-      for (Produit produit : produits){
-        System.out.println(produit);
-       }
-   }
+    public StockBrut() {
+        this.produits = new ArrayList<>();
+    }
 
-   }
+    public void ajouterProduit(Produit produit) {
+        produits.add(produit);
+    }
+
+    public void supprimerProduit(Produit produit) {
+        produits.remove(produit);
+    }
+
+    public int getNombreProduits() {
+        return produits.size();
+    }
+
+    public void afficherStock() {
+        for (Produit produit : produits) {
+            System.out.println(produit);
+        }
+    }
+
+    // (optionnel) un getter pour la liste (utile pour la vue)
+    public ArrayList<Produit> getProduits() {
+        return produits;
+    }
+}
