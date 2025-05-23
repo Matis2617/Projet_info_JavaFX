825422254ff09c487421ba317684f684f073a7d1
Matis2617 <matis.roecker@gmail.com>
2025-05-23 09:26:26 +0200

    Update ChefAtelier.java

--- src/main/java/com/mycompany/projet_fx/ChefAtelier.java
+++ src/main/java/com/mycompany/projet_fx/ChefAtelier.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.projet_fx;
+package com.mycompany.projet_fx.model;
 
 import java.io.Serializable;
 
-/**
- *
- * @author Matis
- */
-public class ChefAtelier extends Personne implements Serializable{
+public class ChefAtelier extends Personne implements Serializable {
 
     public ChefAtelier(String idpersonne, String nom, String prenom) {
         super(idpersonne, nom, prenom);
