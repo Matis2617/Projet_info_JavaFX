87cb3242fceac3ec20d85fada101044b783d7ab5
Matis2617 <matis.roecker@gmail.com>
2025-05-24 12:45:35 +0200

    Update Main.java

--- src/main/java/com/mycompany/projet_fx/Main/Main.java
+++ src/main/java/com/mycompany/projet_fx/Main/Main.java
@@ -1,9 +1,9 @@
 package com.mycompany.projet_fx.Main;
 
-import com.mycompany.projet_fx.View.AtelierView; // Ou AtelierInterface si tu gardes le nom
+import com.mycompany.projet_fx.View.AtelierView;
 
 public class Main {
     public static void main(String[] args) {
-        AtelierView.main(args); // Lance l’application JavaFX
+        AtelierView.main(args);
     }
 }
