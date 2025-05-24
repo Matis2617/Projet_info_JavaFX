2a0c200f92f4c27058e78d44f1cae0f4b66cb727
Matis2617 <matis.roecker@gmail.com>
2025-05-25 01:34:52 +0200

    Update EvenementMaintenance.java

--- src/main/java/com/mycompany/projet_fx/Model/EvenementMaintenance.java
+++ src/main/java/com/mycompany/projet_fx/Model/EvenementMaintenance.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.projet_fx.Model;
 
-/**
- *
- * @author Matis
- */
-public class EvenementMaintenance {
-    
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class EvenementMaintenance implements Serializable {
+    private LocalDateTime dateHeure;
+    private String machine;
+    private String evenement; // "A" ou "D"
+    private String operateur;
+    private String cause;
+
+    public EvenementMaintenance(LocalDateTime dateHeure, String machine, String evenement, String operateur, String cause) {
+        this.dateHeure = dateHeure;
+        this.machine = machine;
+        this.evenement = evenement;
+        this.operateur = operateur;
+        this.cause = cause;
+    }
+
+    public LocalDateTime getDateHeure() { return dateHeure; }
+    public String getMachine() { return machine; }
+    public String getEvenement() { return evenement; }
+    public String getOperateur() { return operateur; }
+    public String getCause() { return cause; }
 }
