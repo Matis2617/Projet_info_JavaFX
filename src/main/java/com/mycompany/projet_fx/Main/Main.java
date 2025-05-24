7c70e2c4b8e7a8c99bf1225d8f400353af927d8d
Matis <Matis@172.17.29.237>
2025-05-24 12:48:39 +0200

    creation depot

--- src/main/java/com/mycompany/projet_fx/Main/Main.java
+++ src/main/java/com/mycompany/projet_fx/Main/Main.java
@@ -7,3 +7,15 @@ public class Main {
         AtelierView.main(args);
     }
 }
+BUILD FAILURE
+------------------------------------------------------------------------
+Total time:  3.383 s
+Finished at: 2025-05-24T12:48:08+02:00
+------------------------------------------------------------------------
+Failed to execute goal org.openjfx:javafx-maven-plugin:0.0.4:run (default-cli) on project Projet_FX: Error: Command execution failed. Process exited with an error: 1 (Exit value: 1) -> [Help 1]
+
+To see the full stack trace of the errors, re-run Maven with the -e switch.
+Re-run Maven using the -X switch to enable full debug logging.
+
+For more information about the errors and possible solutions, please read the following articles:
+[Help 1] http://cwiki.apache.org/confluence/display/MAVEN/MojoExecutionException
