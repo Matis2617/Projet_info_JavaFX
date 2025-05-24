bc0f80d7fbe393e10242f2d1307b33b39e4e9255
Matis2617 <matis.roecker@gmail.com>
2025-05-25 01:38:17 +0200

    Update EvenementMaintenanceController.java

--- src/main/java/com/mycompany/projet_fx/Controller/EvenementMaintenanceController.java
+++ src/main/java/com/mycompany/projet_fx/Controller/EvenementMaintenanceController.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.projet_fx.Controller;
+package com.mycompany.projet_fx.controller;
 
-/**
- *
- * @author Matis
- */
+import com.mycompany.projet_fx.Model.EvenementMaintenance;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EvenementMaintenanceController {
-    
+
+    private ObservableList<EvenementMaintenance> evenements = FXCollections.observableArrayList();
+
+    public ObservableList<EvenementMaintenance> getEvenements() {
+        return evenements;
+    }
+
+    public void ajouterEvenement(EvenementMaintenance evt) {
+        evenements.add(evt);
+    }
+
+    public void supprimerEvenement(EvenementMaintenance evt) {
+        evenements.remove(evt);
+    }
+
+    // Option : Sauvegarde/chargement (binaire)
+    public void sauvegarder(String chemin) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(chemin))) {
+            oos.writeObject(new ArrayList<>(evenements));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public void charger(String chemin) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(chemin))) {
+            evenements.setAll((ArrayList<EvenementMaintenance>) ois.readObject());
+        } catch (Exception e) {
+            // e.printStackTrace();
+        }
+    }
 }
