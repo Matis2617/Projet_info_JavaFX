caa4f3060b677d1bad75703b226cba71989dcbe2
aubinschneider13 <81831754+aubinschneider13@users.noreply.github.com>
2025-05-25 00:29:38 +0200

    Update FiabiliteController.java

--- src/main/java/com/mycompany/projet_fx/Controller/FiabiliteController.java
+++ src/main/java/com/mycompany/projet_fx/Controller/FiabiliteController.java
@@ -4,6 +4,8 @@ import com.mycompany.projet_fx.Model.Fiabilite;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import java.io.*;
+
 public class FiabiliteController {
     private ObservableList<Fiabilite> fiabilites = FXCollections.observableArrayList();
 
@@ -17,5 +19,22 @@ public class FiabiliteController {
 
     public void supprimerFiabilite(Fiabilite fiabilite) {
         fiabilites.remove(fiabilite);
+    }
+
+    public void sauvegarderFiabilites(String fichier) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fichier))) {
+            oos.writeObject(new ArrayList<>(fiabilites));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public void chargerFiabilites(String fichier) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fichier))) {
+            fiabilites.setAll((ArrayList<Fiabilite>) ois.readObject());
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
