58c6e3506fb3e406a223104aa36807f5136c84af
Matis2617 <matis.roecker@gmail.com>
2025-05-24 16:21:55 +0200

    Update MachineFormView.java

--- src/main/java/com/mycompany/projet_fx/View/MachineFormView.java
+++ src/main/java/com/mycompany/projet_fx/View/MachineFormView.java
@@ -3,12 +3,11 @@ package com.mycompany.projet_fx.View;
 import com.mycompany.projet_fx.Model.Machine;
 import com.mycompany.projet_fx.Model.Equipement;
 import com.mycompany.projet_fx.Model.Atelier;
-import com.mycompany.projet_fx.Utils.AtelierSauvegarde; // Si tu as centralisé la sauvegarde ici
+import com.mycompany.projet_fx.Utils.AtelierSauvegarde;
 
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.collections.ObservableList;
-
 import javafx.geometry.Insets;
 
 public class MachineFormView {
@@ -33,9 +32,6 @@ public class MachineFormView {
         TextField coutField = new TextField();
         coutField.setPromptText("Coût");
 
-        TextField tempsField = new TextField();
-        tempsField.setPromptText("Temps préparation");
-
         ComboBox<Machine.ETAT> etatBox = new ComboBox<>();
         etatBox.getItems().addAll(Machine.ETAT.values());
         etatBox.setPromptText("État");
@@ -51,7 +47,6 @@ public class MachineFormView {
                 int absc = Integer.parseInt(abscField.getText());
                 int ord = Integer.parseInt(ordField.getText());
                 float cout = Float.parseFloat(coutField.getText());
-                float temps = Float.parseFloat(tempsField.getText());
                 Machine.ETAT etat = etatBox.getValue();
 
                 if (absc < 0 || absc >= 50 || ord < 0 || ord >= 50) {
@@ -78,7 +73,7 @@ public class MachineFormView {
                 }
                 if (!existe) {
                     Machine m = new Machine(
-                            atelier.getEquipements().size() + 1,
+                            atelier.getEquipements().size() + 1, // id_equipement (unique)
                             id, desc, absc, ord, cout, etat
                     );
                     atelier.getEquipements().add(m);
@@ -98,50 +93,50 @@ public class MachineFormView {
 
         box.getChildren().addAll(
                 new Label("Ajouter une machine :"),
-                idField, descField, abscField, ordField, coutField, tempsField,
+                idField, descField, abscField, ordField, coutField,
                 etatBox, ajouterBtn, retourBtn, erreurLabel
         );
         return box;
     }
+
     public static void modifierMachine(Machine machine, Atelier atelier, String nomFichier, Runnable onRefresh) {
-    Dialog<Void> dlg = new Dialog<>();
-    dlg.setTitle("Modifier la machine");
-    VBox box = new VBox(10);
-    box.setPadding(new Insets(8));
+        Dialog<Void> dlg = new Dialog<>();
+        dlg.setTitle("Modifier la machine");
+        VBox box = new VBox(10);
+        box.setPadding(new Insets(8));
 
-    TextField descField = new TextField(machine.getDmachine());
-    TextField abscField = new TextField(String.valueOf(machine.getAbscisse()));
-    TextField ordField = new TextField(String.valueOf(machine.getOrdonnee()));
-    TextField coutField = new TextField(String.valueOf(machine.getC()));
-    ComboBox<Machine.ETAT> etatBox = new ComboBox<>();
-    etatBox.getItems().addAll(Machine.ETAT.values());
-    etatBox.setValue(machine.getEtat());
+        TextField descField = new TextField(machine.getDmachine());
+        TextField abscField = new TextField(String.valueOf(machine.getAbscisse()));
+        TextField ordField = new TextField(String.valueOf(machine.getOrdonnee()));
+        TextField coutField = new TextField(String.valueOf(machine.getC()));
+        ComboBox<Machine.ETAT> etatBox = new ComboBox<>();
+        etatBox.getItems().addAll(Machine.ETAT.values());
+        etatBox.setValue(machine.getEtat());
 
-    Button valider = new Button("Valider");
-    valider.setOnAction(ev -> {
-        machine.setDmachine(descField.getText());
-        machine.setAbscisse(Integer.parseInt(abscField.getText()));
-        machine.setOrdonnee(Integer.parseInt(ordField.getText()));
-        machine.setC(Float.parseFloat(coutField.getText()));
-        machine.setEtat(etatBox.getValue());
-        // Sauvegarder atelier...
-        // AtelierSauvegarde.sauvegarderAtelier(atelier, nomFichier);
-        dlg.setResult(null);
-        dlg.close();
-        if (onRefresh != null) onRefresh.run();
-    });
+        Button valider = new Button("Valider");
+        valider.setOnAction(ev -> {
+            machine.setDmachine(descField.getText());
+            machine.setAbscisse(Integer.parseInt(abscField.getText()));
+            machine.setOrdonnee(Integer.parseInt(ordField.getText()));
+            machine.setC(Float.parseFloat(coutField.getText()));
+            machine.setEtat(etatBox.getValue());
+            // Sauvegarder atelier...
+            AtelierSauvegarde.sauvegarderAtelier(atelier, nomFichier);
+            dlg.setResult(null);
+            dlg.close();
+            if (onRefresh != null) onRefresh.run();
+        });
 
-    box.getChildren().addAll(
-        new Label("Description :"), descField,
-        new Label("Abscisse :"), abscField,
-        new Label("Ordonnée :"), ordField,
-        new Label("Coût :"), coutField,
-        new Label("État :"), etatBox,
-        valider
-    );
-    dlg.getDialogPane().setContent(box);
-    dlg.getDialogPane().getButtonTypes().add(ButtonType.CLOSE);
-    dlg.showAndWait();
-}
-
+        box.getChildren().addAll(
+            new Label("Description :"), descField,
+            new Label("Abscisse :"), abscField,
+            new Label("Ordonnée :"), ordField,
+            new Label("Coût :"), coutField,
+            new Label("État :"), etatBox,
+            valider
+        );
+        dlg.getDialogPane().setContent(box);
+        dlg.getDialogPane().getButtonTypes().add(ButtonType.CLOSE);
+        dlg.showAndWait();
+    }
 }
